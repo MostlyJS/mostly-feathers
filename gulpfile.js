93445745f478cf7d6097a5a9a8bfb59c78207fea
Fora <luoxuemai@boqii.com>
2017-05-01 21:00:53 +0800

    Add gulp

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const sourcemaps = require('gulp-sourcemaps');
+const changed = require('gulp-changed');
+const eslint = require('gulp-eslint');
+
+gulp.task('lint', function() {
+  return gulp.src('src/**/*.js')
+    .pipe(changed('lib'))
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+});
+
+gulp.task('compile', () => {
+  return gulp.src('src/**/*.js')
+    .pipe(changed('lib'))
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['env']
+    }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('lib'));
+});
+
+gulp.task('watch', () => {
+  gulp.watch('src/**/*.js',['lint', 'compile']);
+});
+
+gulp.task('default', ['watch']);
