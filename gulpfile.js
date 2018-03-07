02168a8ceaba8528a30e7fef28ff09ffbc7d4fd1
forafish <fora.luo@gmail.com>
2018-03-07 12:03:15 +0800

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ gulp.task('lint', function() {
 
 gulp.task('compile', ['lint'], () => {
   return gulp.src('src/**/*.js')
-    // .pipe(changed('lib'))
+    //.pipe(changed('lib'))
     .pipe(sourcemaps.init())
     .pipe(babel())
     .pipe(sourcemaps.write())
