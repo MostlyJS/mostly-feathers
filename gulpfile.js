cc5b80898b35375afb15b0c5a043bbd2a7f0f787
forafish <fora.luo@gmail.com>
2018-04-19 19:11:30 +0800

    remove babel compile from gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,4 @@
 const gulp = require('gulp');
-const babel = require('gulp-babel');
-const sourcemaps = require('gulp-sourcemaps');
 const changed = require('gulp-changed');
 const eslint = require('gulp-eslint');
 
@@ -11,17 +9,8 @@ gulp.task('lint', function () {
     .pipe(eslint.format());
 });
 
-gulp.task('compile', ['lint'], () => {
-  return gulp.src('src/**/*.js')
-    //.pipe(changed('lib'))
-    .pipe(sourcemaps.init())
-    .pipe(babel())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('lib'));
-});
-
 gulp.task('watch', () => {
-  gulp.watch('src/**/*.js', ['lint', 'compile']);
+  gulp.watch('src/**/*.js', ['lint']);
 });
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['watch', 'lint']);
