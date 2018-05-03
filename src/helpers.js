430dec2dee90602af4adfd89f8e4ab339a3ce77f
forafish <fora.luo@gmail.com>
2018-05-03 14:27:33 +0800

    remove convert `null` id

--- src/helpers.js
+++ src/helpers.js
@@ -3,7 +3,6 @@ import fp from 'mostly-func';
 export const defaultMethods = ['find', 'get', 'create', 'update', 'patch', 'remove'];
 
 export const idAction = (id, params) => {
-  if (id === 'null') id = null;
   const action = params && (params.action || (params.query && params.query.$action));
   // check if id is action name for find
   if (id && !action) {
