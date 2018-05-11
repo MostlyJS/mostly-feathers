cfdd4bb6a68c622d680be318db93951dd108db69
forafish <fora.luo@gmail.com>
2018-05-11 10:56:31 +0800

    rename fp.assign to fp.assignAll

--- src/service.js
+++ src/service.js
@@ -11,7 +11,7 @@ const defaultOptions = {
 
 export default class Service {
   constructor (options) {
-    this.options = fp.assign(defaultOptions, options);
+    this.options = fp.assignAll(defaultOptions, options);
     this.name = this.options.name;
   }
 
